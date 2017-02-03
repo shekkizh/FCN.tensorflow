22d7e692880a7b753baa660bd05d9114ad012e89
Sarath Shekkizhar <shekkizh@usc.edu>
2017-02-03 09:32:24 +0530

    Summary api in code updated to latest

--- TensorflowUtils.py
+++ TensorflowUtils.py
@@ -224,16 +224,16 @@ def bottleneck_unit(x, out_chan1, out_chan2, down_stride=False, up_stride=False,
 
 def add_to_regularization_and_summary(var):
     if var is not None:
-        tf.histogram_summary(var.op.name, var)
+        tf.summary.histogram(var.op.name, var)
         tf.add_to_collection("reg_loss", tf.nn.l2_loss(var))
 
 
 def add_activation_summary(var):
     if var is not None:
-        tf.histogram_summary(var.op.name + "/activation", var)
-        tf.scalar_summary(var.op.name + "/sparsity", tf.nn.zero_fraction(var))
+        tf.summary.histogram(var.op.name + "/activation", var)
+        tf.summary.scalar(var.op.name + "/sparsity", tf.nn.zero_fraction(var))
 
 
 def add_gradient_summary(grad, var):
     if grad is not None:
-        tf.histogram_summary(var.op.name + "/gradient", grad)
+        tf.summary.histogram(var.op.name + "/gradient", grad)

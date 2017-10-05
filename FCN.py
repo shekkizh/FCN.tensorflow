3a445336612e861c96c1e82540ebf34c1ec1cea7
Sandeep Dcunha <sdcunha@umass.edu>
2017-10-05 10:36:31 -0400

    Change no. classes.

--- FCN.py
+++ FCN.py
@@ -20,7 +20,7 @@ tf.flags.DEFINE_string('mode', "train", "Mode train/ test/ visualize")
 MODEL_URL = 'http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat'
 
 MAX_ITERATION = int(1e5 + 1)
-NUM_OF_CLASSESS = 151
+NUM_OF_CLASSESS = 2
 IMAGE_SIZE = 224
 
 
